4ac71c4ef5fd638be31a225871be997a62fb38b1
Imashapathum <108999394+Imashapathum2003@users.noreply.github.com>
2022-07-12 13:42:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['96778639556'] //ur owner number
+global.ownername = "🦄Imasha Pathum🕊️" //ur owner name
+global.ytname = "YT: 𝚂𝙻 𝚃𝙴𝙲𝙷 𝚃𝙸𝙿𝚂" //ur yt chanel name
+global.socialm = "GitHub: Imashapathum2003" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = ['94778639556']
+global.ownertag = '94778639556' //ur tag number
+global.botname = 'Lakiya Md' //ur bot name
+global.linkz = "https://vt.tiktok.com/ZSRetngaT/" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = 'https://github.com/Imashapathum2003/CheemsBot-MD4' //script link
+global.reactmoji = "🧑‍💻" //ur menu react emoji
+global.themeemoji = "🙋‍♂️" //ur theme emoji
+global.packname = "🧑‍💻 Lakiya Md🕊️ " //ur sticker watermark packname
+global.author = "🦄Imasha Pathum" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
